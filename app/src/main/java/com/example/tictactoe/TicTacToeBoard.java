6ff933aea21bc23dd5ad598e98f255fd85da54bd
sahil <sahilasarkar3@gmail.com>
2024-06-01 05:51:35 +0530

    Game login & user interaction

--- app/src/main/java/com/example/tictactoe/TicTacToeBoard.java
+++ app/src/main/java/com/example/tictactoe/TicTacToeBoard.java
@@ -5,6 +5,7 @@ import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
 import android.view.View;
 
 import androidx.annotation.Nullable;
@@ -16,11 +17,13 @@ public class TicTacToeBoard extends View {
     private final int Ocolor;
     private final int winningLineColor;
     private final Paint paint = new Paint();
+
+    private final GameLogic game;
     private int cellsize = getWidth()/3;
 
     public TicTacToeBoard(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
-
+game = new GameLogic();
         TypedArray a = context.getTheme().obtainStyledAttributes(attrs,R.styleable.TicTacToeBoard,0,0);
 
         try {
@@ -50,6 +53,36 @@ public class TicTacToeBoard extends View {
         paint.setAntiAlias(true);
 
         drawGameBoard(canvas);
+        drawMarkers(canvas);
+
+
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event){
+    float x = event.getX();
+    float y = event.getY();
+
+    int action = event.getAction();
+    if (action == MotionEvent.ACTION_DOWN){
+        int row= (int) Math.ceil(y/cellsize);
+        int col= (int) Math.ceil(x/cellsize);
+
+        if (game.updateGameBoard(row,col)){
+            invalidate();
+            if (game.getPlayer() % 2 == 0){
+                game.setPlayer(game.getPlayer()-1);
+            }
+            else {
+                game.setPlayer(game.getPlayer()+1);
+            }
+        }
+
+        invalidate();
+        return true;
+     }
+
+      return false;
     }
 
     private void drawGameBoard(Canvas canvas) {
@@ -61,5 +94,43 @@ canvas.drawLine(cellsize*c,0,cellsize*c,canvas.getWidth(), paint);
         for(int r=1;r<3;r++){
             canvas.drawLine(0,cellsize*r,canvas.getWidth(),cellsize*r, paint);
         }
+    }
+
+    private void drawMarkers(Canvas canvas){
+        for (int r=0;r<3;r++){
+            for (int c=0;c<3;c++){
+              if (game.getGameBoard()[r][c] != 0){
+                if (game.getGameBoard()[r][c] == 1){
+                    drawX(canvas,r,c);
+                }
+                else {
+                    drawO(canvas,r,c);
+                }
+              }
+            }
+        }
+    }
+
+    private void drawX(Canvas canvas,int row, int col){
+        paint.setColor(Xcolor);
+        canvas.drawLine((float) ((col+1)*cellsize - cellsize *0.2),
+                (float) (row*cellsize+ cellsize *0.2),
+                (float) (col*cellsize+ cellsize *0.2),
+                (float) ((row+1)*cellsize-cellsize *0.2),paint);
+
+        canvas.drawLine((float) (col*cellsize+ cellsize *0.2),
+                (float) (row*cellsize+ cellsize *0.2),
+                (float) ((col+1)*cellsize- cellsize *0.2),
+                (float) ((row+1)*cellsize- cellsize *0.2),
+                paint);
+    }
+
+    private void drawO(Canvas canvas,int row, int col){
+        paint.setColor(Ocolor);
+        canvas.drawOval((float) (col*cellsize+ cellsize *0.2),
+                (float) (row*cellsize+ cellsize *0.2),
+                (float) ((col*cellsize+cellsize)- cellsize *0.2),
+                (float) ((row*cellsize+cellsize)- cellsize *0.2),
+                paint);
     }
 }
